64768de48d3204ddb0935aae84eca00f994592b2
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-17 16:47:50 +0100

    variabili istanza+construct

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 
+class Movie 
+{
+    public $title;
+    public $year;
+    public $language;
+    public $genre;
+    public $director; 
+
+
+
+    public function __construct($title, $year, $language, $genre, $director){
+        $this->title = $title;
+        $this->year = $year;
+        $this->language = $language;
+        $this->genre = $genre;
+        $this->director = $director;
+    }
+}
+
+
+$back_to_the_future = new Movie('Back to the Future', 1985,'english', 'sci-fi', 'Robert Zemeckis');
+
+var_dump($back_to_the_future);
+
+
 ?>
 
 <!DOCTYPE html>
