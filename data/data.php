4dbe5c5864364d5d21bfd31c2055028864bd5df2
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-20 17:34:19 +0100

    prova doppio genere

--- data/data.php
+++ data/data.php
@@ -11,7 +11,7 @@ $western = new Genre('western');
 
 $back_to_the_future = new Movie('Back to the Future', 1985,'english', $scifi, 'Robert Zemeckis');
 $back_to_the_future_2 = new Movie('Back to the Future Part II', 1989,'english', $scifi, 'Robert Zemeckis');
-$back_to_the_future_3 = new Movie('Back to the Future Part III', 1990,'english', $scifi, $western, 'Robert Zemeckis');
+$back_to_the_future_3 = new Movie('Back to the Future Part III', 1990,'english', [$scifi, $western], 'Robert Zemeckis');
 
 $movies = [$back_to_the_future, $back_to_the_future_2, $back_to_the_future_3];
 
